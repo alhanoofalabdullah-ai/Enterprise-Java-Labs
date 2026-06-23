dfc4bd1e459c124faeae85984e7f89791ddf242a
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 22:55:33 +0300

    Create Lead.java

---
+++ project-02-enterprise-customer-relationship-management-system/Lead.java
@@ -0,0 +1,15 @@
+package src.models;
+
+public class Lead {
+
+    private int id;
+    private String company;
+
+    public Lead(
+            int id,
+            String company) {
+
+        this.id = id;
+        this.company = company;
+    }
+}
