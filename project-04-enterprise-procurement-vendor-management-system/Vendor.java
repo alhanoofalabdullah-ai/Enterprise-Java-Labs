6d8d1a121959b9fdc5f8fa99b847ae85a93ccd1c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 00:23:14 +0300

    Create Vendor.java

---
+++ project-04-enterprise-procurement-vendor-management-system/Vendor.java
@@ -0,0 +1,15 @@
+package src.models;
+
+public class Vendor {
+
+    private int id;
+    private String vendorName;
+
+    public Vendor(
+            int id,
+            String vendorName) {
+
+        this.id = id;
+        this.vendorName = vendorName;
+    }
+}
