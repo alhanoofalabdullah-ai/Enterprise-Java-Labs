ff38035f6b39b6ae29f4b046a60ac40f19112179
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-23 22:48:41 +0300

    Create Employee.java

---
+++ project-01-enterprise-employee-management-system/Employee.java
@@ -0,0 +1,22 @@
+package src.models;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private String department;
+
+    public Employee(
+            int id,
+            String name,
+            String department) {
+
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
