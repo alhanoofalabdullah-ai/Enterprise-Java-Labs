3de4d7376ff3fed905f2bcc3634fc7fb643600a6
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:17:04 +0300

    Create AccessControlService.java

---
+++ project-19-enterprise-visitor-management-access-control-system/AccessControlService.java
@@ -0,0 +1,67 @@
+package src.services;
+
+import src.models.AccessPass;
+import src.models.VisitRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccessControlService {
+
+    private VisitService visitService;
+    private List<AccessPass> passes = new ArrayList<>();
+
+    public AccessControlService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    public void approveVisit(int visitId) {
+
+        VisitRequest visit =
+                visitService.findVisit(visitId);
+
+        if (visit != null) {
+            visit.approve();
+        }
+    }
+
+    public void issuePass(int visitId) {
+
+        VisitRequest visit =
+                visitService.findVisit(visitId);
+
+        if (visit != null
+                && visit.getStatus().equals("APPROVED")) {
+
+            passes.add(
+                    new AccessPass(
+                            visitId,
+                            "PASS-" + visitId
+                    )
+            );
+        }
+    }
+
+    public void checkIn(int visitId) {
+
+        VisitRequest visit =
+                visitService.findVisit(visitId);
+
+        if (visit != null) {
+            visit.checkIn();
+        }
+    }
+
+    public void checkOut(int visitId) {
+
+        VisitRequest visit =
+                visitService.findVisit(visitId);
+
+        if (visit != null) {
+            visit.checkOut();
+        }
+    }
+
+    public int totalPasses() {
+        return passes.size();
+    }
+}
