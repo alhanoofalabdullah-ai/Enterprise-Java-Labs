11efc0f480f9012bde9eec049844b651b9391d7c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:15:15 +0300

    Create AccessPass.java

---
+++ project-19-enterprise-visitor-management-access-control-system/AccessPass.java
@@ -0,0 +1,20 @@
+package src.models;
+
+public class AccessPass {
+
+    private int visitId;
+    private String passCode;
+
+    public AccessPass(int visitId, String passCode) {
+        this.visitId = visitId;
+        this.passCode = passCode;
+    }
+
+    public int getVisitId() {
+        return visitId;
+    }
+
+    public String getPassCode() {
+        return passCode;
+    }
+}
