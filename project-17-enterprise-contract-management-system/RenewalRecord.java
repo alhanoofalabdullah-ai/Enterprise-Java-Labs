0c1dfae3e1b88f61b46bbe38df6158432c4dcd07
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 12:52:38 +0300

    Create RenewalRecord.java

---
+++ project-17-enterprise-contract-management-system/RenewalRecord.java
@@ -0,0 +1,12 @@
+package src.models;
+
+public class RenewalRecord {
+
+    private int contractId;
+    private String notes;
+
+    public RenewalRecord(int contractId, String notes) {
+        this.contractId = contractId;
+        this.notes = notes;
+    }
+}
