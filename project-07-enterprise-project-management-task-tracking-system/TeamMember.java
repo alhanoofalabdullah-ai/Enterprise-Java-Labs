2868be279ef6717fdbba469677e2653fd0ed8ca6
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 05:19:19 +0300

    Create TeamMember.java

---
+++ project-07-enterprise-project-management-task-tracking-system/TeamMember.java
@@ -0,0 +1,18 @@
+package src.models;
+
+public class TeamMember {
+
+    private int id;
+    private String name;
+    private String role;
+
+    public TeamMember(
+            int id,
+            String name,
+            String role) {
+
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+}
