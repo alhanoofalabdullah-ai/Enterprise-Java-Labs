0d7a7bd416030a142ddfed204c0d641aabc8da22
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:29:11 +0300

    Create ProjectService.java

---
+++ project-20-enterprise-project-portfolio-management-system/ProjectService.java
@@ -0,0 +1,19 @@
+package src.services;
+
+import src.models.Project;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectService {
+
+    private List<Project> projects = new ArrayList<>();
+    private int nextId = 1;
+
+    public void createProject(String name) {
+        projects.add(new Project(nextId++, name));
+    }
+
+    public int totalProjects() {
+        return projects.size();
+    }
+}
