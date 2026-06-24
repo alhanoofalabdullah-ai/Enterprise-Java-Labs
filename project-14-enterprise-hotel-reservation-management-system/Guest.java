4d810f12f5164f2b5fe2b7524eb7b7685447da07
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 12:11:20 +0300

    Create Guest.java

---
+++ project-14-enterprise-hotel-reservation-management-system/Guest.java
@@ -0,0 +1,14 @@
+package src.models;
+
+public class Guest {
+
+    private int id;
+    private String name;
+    private String phone;
+
+    public Guest(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
