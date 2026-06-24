9c331d9f807eb9443c4c7a06a8888b9bec3e8110
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 10:38:17 +0300

    Create AuditLog.java

---
+++ project-10-enterprise-document-management-workflow-system/AuditLog.java
@@ -0,0 +1,14 @@
+package src.models;
+
+public class AuditLog {
+
+    private String action;
+
+    public AuditLog(String action) {
+        this.action = action;
+    }
+
+    public String getAction() {
+        return action;
+    }
+}
