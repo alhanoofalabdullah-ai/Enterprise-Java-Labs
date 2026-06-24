8717efe18f42db858a61a7e8ff1b114e29476f05
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 04:52:51 +0300

    Create Agent.java

---
+++ project-06-enterprise-helpdesk-it-service-management-system/Agent.java
@@ -0,0 +1,15 @@
+package src.models;
+
+public class Agent {
+
+    private int id;
+    private String name;
+
+    public Agent(
+            int id,
+            String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+}
