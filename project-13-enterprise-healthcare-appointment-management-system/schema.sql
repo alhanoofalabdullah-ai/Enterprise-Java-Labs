43eb0619741a93ec23422d271efafe33fe741440
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 11:52:29 +0300

    Create schema.sql

---
+++ project-13-enterprise-healthcare-appointment-management-system/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE patients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_name VARCHAR(150),
+    phone VARCHAR(50)
+);
+
+CREATE TABLE doctors (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    doctor_name VARCHAR(150),
+    specialty VARCHAR(150)
+);
+
+CREATE TABLE appointments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_name VARCHAR(150),
+    doctor_name VARCHAR(150),
+    department VARCHAR(150),
+    appointment_date DATE,
+    status VARCHAR(50)
+);
