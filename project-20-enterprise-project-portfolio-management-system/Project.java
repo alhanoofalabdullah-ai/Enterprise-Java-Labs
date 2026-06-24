38609c9f1ae33e278a4e89e750a8571141f9f393
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:27:02 +0300

    Create Project.java

---
+++ project-20-enterprise-project-portfolio-management-system/Project.java
@@ -0,0 +1,14 @@
+package src.models;
+
+public class Project {
+
+    private int id;
+    private String name;
+    private String status;
+
+    public Project(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.status = "ACTIVE";
+    }
+}
