69224028247cc96820f7556558005c0a5177789b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 11:20:48 +0300

    Create Certificate.java

---
+++ project-12-enterprise-learning-management-system/Certificate.java
@@ -0,0 +1,12 @@
+package src.models;
+
+public class Certificate {
+
+    private String studentName;
+    private String courseTitle;
+
+    public Certificate(String studentName, String courseTitle) {
+        this.studentName = studentName;
+        this.courseTitle = courseTitle;
+    }
+}
